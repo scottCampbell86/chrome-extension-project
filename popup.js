f23d523e3cfc7f22027c264c24564c0fef1d4a01
Chris Lung <chrislung@Chriss-MBP.verizon.net>
2020-10-02 23:47:51 -0400

    Updated

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
+//This lives in Chrome Dev Tools
 console.log('here');
-e
-const button = document.querySelector('#timer-button');
-button.addEventListener('click', () => console.log('click heard'));
+
+// const button = document.querySelector('#timer-button');
+// button.addEventListener('click', () => console.log('click heard'));
+
